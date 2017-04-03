ce63a2d1b0ef150c6b777d714802cb96f5b2b0ff
Jonathan Tilden <jtilden20@gmail.com>
2017-04-03 11:28:40 -0700

    Updated per-file license info

--- DDConvexHullCmd.h
+++ DDConvexHullCmd.h
@@ -1,10 +1,30 @@
 //
-//  DDConvexHullCmd.h
-//  DDConvexHull
+// DDConvexHullCmd.h
+// DDConvexHull
 //
-//  Created by Jonathan Tilden on 1/1/13.
-//  Copyright (c) 2013 Jonathan Tilden. All rights reserved.
+// Created by Jonathan Tilden on 1/1/13.
 //
+// MIT License
+//
+// Copyright (c) 2017 Jonathan Tilden
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in 
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 #ifndef DDConvexHull_DDConvexHullCmd_h
 #define DDConvexHull_DDConvexHullCmd_h
--- DDConvexHullNode.h
+++ DDConvexHullNode.h
@@ -1,10 +1,30 @@
 //
-//  DDConvexHullNode.h
-//  DDConvexHull
+// DDConvexHullNode.h
+// DDConvexHull
 //
-//  Created by Jonathan Tilden on 12/30/12.
-//  Copyright (c) 2012 Jonathan Tilden. All rights reserved.
+// Created by Jonathan Tilden on 12/30/12.
 //
+// MIT License
+//
+// Copyright (c) 2017 Jonathan Tilden
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in 
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 #ifndef __DDConvexHull__DDConvexHullNode__
 #define __DDConvexHull__DDConvexHullNode__
